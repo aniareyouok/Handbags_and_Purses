19c6bf40e4ec442005ca65b0f27e38af952acfcb
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-16 17:05:12 +0100

    created product component and added gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import Button from './components/Button';
+import bag_1 from './assets/bag_1.png';
+import bag_2 from './assets/bag_2.png';
+import bag_3 from './assets/bag_3.png';
+import bag_4 from './assets/bag_4.png';
 
 function App() {
   return (
       <Fragment>
       <h1>Handbags & Purses</h1>
+
+          {/*Navigation bar with buttons*/}
           <nav>
               <Button>
                   name="to the collection"
@@ -23,6 +29,39 @@ function App() {
                   disabled={true}
               </Button>
           </nav>
+
+          {/*Product gallery*/}
+          {/*Note that images are imported, see line 4 to 7*/}
+          <main>
+              <Product
+                  sticker="Best seller"
+                  image={bag_1}
+                  imgtext="picture of bag"
+                  title="The handy bag"
+                  price="400"
+              />
+              <Product
+                  sticker="Best seller"
+                  image={bag_2}
+                  imgtext="picture of bag"
+                  title="The stylish bag"
+                  price="250"
+              />
+              <Product
+                  sticker="Best seller"
+                  image={bag_3}
+                  imgtext="picture of bag"
+                  title="The simple bag"
+                  price="300"
+              />
+              <Product
+                  sticker="Best seller"
+                  image={bag_4}
+                  imgtext="picture of bag"
+                  title="The trendy bag"
+                  price="150"
+              />
+          </main>
         </Fragment>
   )}
 
